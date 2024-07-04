e55f8cf71052529b47b2996c5183a7593e78e589
Houtamelo <antoniopedrogf@hotmail.com>
2024-07-03 23:51:03 -0300

    Changed unsafe closure to accept &[&Variant] array

--- src/connect_deferred.rs
+++ src/connect_deferred.rs
@@ -1,35 +1,37 @@
 use crate::prelude::*;
 
-struct UnsafeFn<F: FnMut()>(F);
+struct UnsafeFn<F>(F);
 
-impl<F: FnMut()> UnsafeFn<F> {
-	fn invoke(&mut self) {
-		self.0();
+impl<F: FnMut(&[&Variant])> UnsafeFn<F> {
+	fn invoke(&mut self, args: &[&Variant]) {
+		self.0(args);
 	}
 }
 
-unsafe impl<F: FnMut()> Send for UnsafeFn<F> {}
-unsafe impl<F: FnMut()> Sync for UnsafeFn<F> {}
+unsafe impl<F> Send for UnsafeFn<F> {}
+unsafe impl<F> Sync for UnsafeFn<F> {}
 
 enum FlexCallable {
 	Callable(Callable),
-	Closure(UnsafeFn<Box<dyn FnMut()>>),
+	#[allow(clippy::type_complexity)]
+	Closure(UnsafeFn<Box<dyn FnMut(&[&Variant])>>),
 }
 
 impl From<FlexCallable> for Callable {
 	fn from(value: FlexCallable) -> Self {
 		match value {
 			FlexCallable::Callable(callable) => callable,
-			FlexCallable::Closure(mut unsafe_fn) => 
-				Callable::from_fn("lambda", move |_| {
-					unsafe_fn.invoke();
+			FlexCallable::Closure(mut unsafe_fn) => {
+				Callable::from_fn("lambda", move |args| {
+					unsafe_fn.invoke(args);
 					Ok(Variant::nil())
-				}),
+				})
+			}
 		}
 	}
 }
 
-impl<T: FnMut() + 'static> From<T> for FlexCallable {
+impl<T: FnMut(&[&Variant]) + 'static> From<T> for FlexCallable {
 	fn from(value: T) -> Self {
 		FlexCallable::Closure(UnsafeFn(Box::new(value)))
 	}
@@ -51,10 +53,10 @@ impl<T: GodotClass + Inherits<Object>> ConnectDeferred for Gd<T> {
 	fn connect_deferred(&mut self, signal: impl Into<StringName>, callable: impl Into<FlexCallable>) {
 		let signal = signal.into();
 		let callable = callable.into();
-		
+
 		self.upcast_mut()
-			.connect_ex(signal, callable.into())
-			.flags(ConnectFlags::DEFERRED.ord() as u32)
-			.done();
+		    .connect_ex(signal, callable.into())
+		    .flags(ConnectFlags::DEFERRED.ord() as u32)
+		    .done();
 	}
 }
