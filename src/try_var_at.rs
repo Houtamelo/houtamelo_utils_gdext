ce0922ecd0613c8c6babd49e7499e7fea0ff2eeb
Houtamelo <antoniopedrogf@hotmail.com>
2024-12-14 05:36:45 -0300

    0.4.5: Impl try_var_at for slices

--- src/try_var_at.rs
+++ src/try_var_at.rs
@@ -39,3 +39,39 @@ impl TryVarAt<usize> for VariantArray {
 			})
 	}
 }
+
+impl TryVarAt<usize> for &[Variant] {
+	fn try_var_at<Var: FromGodot>(&self, index: usize) -> Result<Var> {
+		self.get(index)
+			.ok_or_else(|| {
+				anyhow!("Index `{index}` is out of bounds. Array length: `{}`", self.len())
+			})
+			.and_then(|val| {
+				val.try_to::<Var>().map_err(|err| {
+					anyhow!(
+						"Index `{index}` is in bounds, but value is not of expected type `{}`.\n\
+						 Conversion Error: {err:?}",
+						std::any::type_name::<Var>()
+					)
+				})
+			})
+	}
+}
+
+impl TryVarAt<usize> for &[&Variant] {
+	fn try_var_at<Var: FromGodot>(&self, index: usize) -> Result<Var> {
+		self.get(index)
+			.ok_or_else(|| {
+				anyhow!("Index `{index}` is out of bounds. Array length: `{}`", self.len())
+			})
+			.and_then(|val| {
+				val.try_to::<Var>().map_err(|err| {
+					anyhow!(
+						"Index `{index}` is in bounds, but value is not of expected type `{}`.\n\
+						 Conversion Error: {err:?}",
+						std::any::type_name::<Var>()
+					)
+				})
+			})
+	}
+}
