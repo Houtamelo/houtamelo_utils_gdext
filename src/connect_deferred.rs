91fd4c48a6dfcccd8aa941fbd03adc771cdb8e3a
Houtamelo <antoniopedrogf@hotmail.com>
2025-05-01 12:35:11 -0300

    0.5.0: - Add `connect_deferred` and `connect_self_deferred`

--- src/connect_deferred.rs
+++ src/connect_deferred.rs
@@ -7,7 +7,12 @@ use godot::{
 
 use crate::internal::*;
 
-pub trait ConnectDeferred<'a, Ps: meta::ParamTuple + InParamTuple + 'static> {
+pub trait ConnectDeferred<'a, C, Ps: meta::ParamTuple + InParamTuple + 'static> {
+    fn connect_deferred<F: SignalReceiver<(), Ps>>(&'a mut self, f: F);
+
+    fn connect_self_deferred<F: for<'b> SignalReceiver<&'b mut C, Ps>>(&'a mut self, f: F)
+    where C: GodotClass<Declarer = DeclUser>;
+
     fn connect_user_deferred<
         Listener: GodotClass<Declarer = DeclUser> + WithBaseField,
         F: for<'b> SignalReceiver<&'b mut Listener, Ps>,
@@ -36,9 +41,25 @@ pub trait ConnectDeferred<'a, Ps: meta::ParamTuple + InParamTuple + 'static> {
     );
 }
 
-impl<'a, C: WithSignals, Ps: meta::ParamTuple + InParamTuple + 'static> ConnectDeferred<'a, Ps>
+impl<'a, C: WithSignals, Ps: meta::ParamTuple + InParamTuple + 'static> ConnectDeferred<'a, C, Ps>
     for TypedSignal<'a, C, Ps>
 {
+    fn connect_deferred<F: SignalReceiver<(), Ps>>(&'a mut self, f: F) {
+        self.connect_builder()
+            .function(f)
+            .flags(ConnectFlags::DEFERRED)
+            .done();
+    }
+
+    fn connect_self_deferred<F: for<'b> SignalReceiver<&'b mut C, Ps>>(&'a mut self, f: F)
+    where C: GodotClass<Declarer = DeclUser> {
+        self.connect_builder()
+            .object_self()
+            .method_mut(f)
+            .flags(ConnectFlags::DEFERRED)
+            .done();
+    }
+
     fn connect_user_deferred<
         Listener: GodotClass<Declarer = DeclUser> + WithBaseField,
         F: for<'b> SignalReceiver<&'b mut Listener, Ps>,
@@ -100,6 +121,9 @@ mod must_compile {
         base: Base<Node>,
     }
 
+    #[godot_api]
+    impl MyNode {}
+
     fn test1(my_node: &MyNode) {}
 
     fn test2(mut my_node: &mut MyNode) {
@@ -109,6 +133,16 @@ mod must_compile {
             .signals()
             .child_entered_tree()
             .connect_obj(&gd, |node: &mut MyNode, child| {});
+
+        my_node
+            .signals()
+            .child_entered_tree()
+            .connect_self_deferred(|this: &mut MyNode, child| {});
+
+        my_node
+            .signals()
+            .child_entered_tree()
+            .connect_deferred(|child| {});
 
         let mut base_gd = gd.clone().upcast::<Node>();
         base_gd
